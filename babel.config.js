c9f4186721bbff76902a7282009b79dfeb7dd452
julius turunen <julius.turunen@aalto.fi>
2021-03-14 23:15:09 +0200

    Add material-ui icons

--- babel.config.js
+++ babel.config.js
@@ -30,6 +30,10 @@ module.exports = {
             "@material-ui/core": {
               "transform": "@material-ui/core/esm/${member}",
               "preventFullImport": true
+            },
+            "@material-ui/icons": {
+              "transform": "@material-ui/icons/esm/${member}",
+              "preventFullImport": true
             }
           }
         ]
