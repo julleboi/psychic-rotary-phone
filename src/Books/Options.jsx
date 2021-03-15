464a01fa25925c5b598d0be845e6b10eadb8cff6
julius turunen <julius.turunen@aalto.fi>
2021-03-15 16:05:13 +0200

    Update field widths

--- src/Books/Options.jsx
+++ src/Books/Options.jsx
@@ -24,42 +24,40 @@ function Options({
         <Grid 
           container
           direction="row"
-          spacing={4}
+          spacing={2}
         >
           <Grid item xs>
             <Typography color="primary">
               Options
             </Typography>
           </Grid>
-          <Grid item container xs={6} spacing={2}>
-            <Grid item xs={6}>
-              <FormControl margin="dense" variant="outlined" fullWidth>
-                <FormLabel id="viewLabel">View as</FormLabel>
-                <Select
-                  id="view"
-                  labelId="viewLabel"
-                  value={currentView}
-                  onChange={onChangeView}
-                >
-                  { views.map((v, i) => 
-                    <MenuItem key={i} value={v}>{v}</MenuItem>) }
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid item xs={6}>
-              <FormControl margin="dense" variant="outlined" fullWidth>
-                <FormLabel id="sortLabel">Sort by</FormLabel>
-                <Select
-                  id="sort"
-                  labelId="sortLabel"
-                  value={currentSortBy}
-                  onChange={onChangeSortBy}
-                >
-                  { sortBy.map((v, i) => 
-                    <MenuItem key={i} value={v}>{v}</MenuItem>) }
-                </Select>
-              </FormControl>
-            </Grid>
+          <Grid item xs={4}>
+            <FormControl margin="dense" variant="outlined" fullWidth>
+              <FormLabel id="viewLabel">View as</FormLabel>
+              <Select
+                id="view"
+                labelId="viewLabel"
+                value={currentView}
+                onChange={onChangeView}
+              >
+                { views.map((v, i) => 
+                  <MenuItem key={i} value={v}>{v}</MenuItem>) }
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={4}>
+            <FormControl margin="dense" variant="outlined" fullWidth>
+              <FormLabel id="sortLabel">Sort by</FormLabel>
+              <Select
+                id="sort"
+                labelId="sortLabel"
+                value={currentSortBy}
+                onChange={onChangeSortBy}
+              >
+                { sortBy.map((v, i) => 
+                  <MenuItem key={i} value={v}>{v}</MenuItem>) }
+              </Select>
+            </FormControl>
           </Grid>
         </Grid>
       </Box>
